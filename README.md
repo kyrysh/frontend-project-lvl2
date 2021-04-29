72cf2bf5579b790c8302c1f70e398c8473644f5e
kyrysh <victoria.tyagleeva@gmail.com>
2021-04-29 10:32:14 +0300

    Step 3: adding src/index.js; modifying bin/gendiff.js, package.json, README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/kyrysh/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/kyrysh/frontend-project-lvl2/actions)
+
+
+[![asciicast](https://asciinema.org/a/mGx2evijvyPh0Nm2UcBUWaDNp.svg)](https://asciinema.org/a/mGx2evijvyPh0Nm2UcBUWaDNp)
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getFullFilePath = (filepath) => path.resolve(process.cwd(), filepath);
+const getJSONfileContent = (fullFilePath) => JSON.parse(fs.readFileSync(fullFilePath));
+
+const genDiff = (filepath1, filepath2) => {
+  const fullFilePath1 = getFullFilePath(filepath1);
+  const fullFilePath2 = getFullFilePath(filepath2);
+  const file1Content = getJSONfileContent(filepath1);
+  const file2Content = getJSONfileContent(filepath2);
+  const file1Keys = _.keys(file1Content);
+  const file2Keys = _.keys(file2Content);
+  const keys = _.union(file1Keys, file2Keys).sort();
+
+  const diffs = keys.reduce((acc, key) => {
+    const value1 = file1Content[key];
+    const value2 = file2Content[key];
+    if (!_.has(file1Content, key)) {
+      return [...acc, `  + ${key}: ${file2Content[key]}`];
+    } else if (!_.has(file2Content, key)) {
+      return [...acc, `  - ${key}: ${value1}`];
+    } else if (value1 !== value2) {
+      return [...acc, `  - ${key}: ${value1}`, `  - ${key}: ${value2}`];
+    } else {
+      return [...acc, `    ${key}: ${value1}`];
+    }
+  }, []);
+
+  const result = `{\n${diffs.join('\n')}\n}`;
+  return result;
+};
+
+export default genDiff;

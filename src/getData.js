41861ed4fc01deff63c0e0ae0be55f0aa7ca5394
kyrysh <victoria.tyagleeva@gmail.com>
2021-05-24 10:42:03 +0300

    Step 6: modifiying getData.js

--- src/getData.js
+++ src/getData.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
-// import path from 'path';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path';
+// import path, { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default (filepath) => {
-  // const fullPath = path.resolve(process.cwd(), filepath);
-  const fullPath = path.join(__dirname, '..', '__fixtures__', filepath);
+  const fullPath = path.resolve(process.cwd(), filepath);
+  // const fullPath = path.join(__dirname, '..', '__fixtures__', filepath);
   const data = fs.readFileSync(fullPath);
   const format = path.extname(fullPath, 'utf8');
   return [data, format];

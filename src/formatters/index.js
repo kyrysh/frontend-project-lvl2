13eef0036ee937bd33b0f0873ccb1cd0902f8a76
kyrysh <victoria.tyagleeva@gmail.com>
2021-05-25 11:57:44 +0300

    Step 8: Adding formatter json

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formatters = {
   stylish,
   plain,
+  json,
 };
 
 export default (data, formatter) => {

c109374a0733c111687f4f7d1d82dfd8a4f385eb
kyrysh <victoria.tyagleeva@gmail.com>
2021-05-25 14:07:11 +0300

    Step 8: modifying genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,12 +3,9 @@ import parse from './parsers.js';
 import buildAST from './buildAST.js';
 import formatData from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2, formatterType = 'stylish') => {
+export default (filepath1, filepath2, formatterType = 'stylish') => {
   const file1ParsedData = parse(getData(filepath1));
   const file2ParsedData = parse(getData(filepath2));
   const AST = buildAST(file1ParsedData, file2ParsedData);
-  // return AST;
   return formatData(AST, formatterType);
 };
-
-export default genDiff;

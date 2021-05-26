3b5a0cc88506bab95aff568172d25b5dab17b5c9
kyrysh <victoria.tyagleeva@gmail.com>
2021-05-26 13:22:54 +0300

    Step 8: modifying bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import program from 'commander';
 import genDiff from '../src/genDiff.js';
-
-const program = new Command();
 
 program
   .description('Compares two JSON files and shows a difference.')
